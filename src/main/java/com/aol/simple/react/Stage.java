635b2ae08d71a3340173e2abc245409a330b8c02
John McClean <john.mcclean@teamaol.com>
2014-12-16 17:33:28 +0000

    organise imports

--- src/main/java/com/aol/simple/react/Stage.java
+++ src/main/java/com/aol/simple/react/Stage.java
@@ -7,7 +7,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
 import java.util.Queue;
-import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ExecutionException;
