ad96c20a80875e38a70107c17acc76ee83bdfd68
John McClean <john.mcclean@teamaol.com>
2014-12-22 11:33:21 +0000

    unit tests for stream conversion

--- src/test/java/com/aol/simple/react/StreamTest.java
+++ src/test/java/com/aol/simple/react/StreamTest.java
@@ -1,5 +1,54 @@
 package com.aol.simple.react;
 
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ExecutionException;
+
+import org.junit.Test;
+
 public class StreamTest {
 
+	@Test
+	public void testStreamFrom() throws InterruptedException,
+			ExecutionException {
+		
+		
+		List<String> strings = new SimpleReact()
+								.<String>fromStream(new SimpleReact()
+												.<Integer> react(() -> 1, () -> 2, () -> 3)
+												.with(it -> "*" + it).stream())
+								.then(it ->  it + "*")
+								.block();
+
+		assertThat(strings.size(), is(3));
+		
+		
+		assertThat(strings,hasItem("*1*"));
+
+	}
+	@Test
+	public void testStreamOf() throws InterruptedException,
+			ExecutionException {
+		
+		
+		List<String> strings = new SimpleReact()
+								.<String>fromStream(new SimpleReact()
+												.<Integer> react(() -> 1, () -> 2, () -> 3)
+												.then(it -> "*" + it).stream())
+								.then(it ->  it + "*")
+								.block();
+
+		assertThat(strings.size(), is(3));
+		
+		
+		assertThat(strings,hasItem("*1*"));
+
+	}
+	
 }
