d7f7efff87e620b0c0f70c454acdd01834c7f149
John McClean <john.mcclean@teamaol.com>
2015-03-29 00:09:56 +0000

    push data into Streams

---
+++ src/main/java/com/aol/simple/react/stream/pushable/MultiplePushableStreamsBuilder.java
@@ -0,0 +1,82 @@
+package com.aol.simple.react.stream.pushable;
+
+import java.util.stream.Stream;
+
+import org.jooq.lambda.Seq;
+
+import com.aol.simple.react.async.Queue;
+import com.aol.simple.react.async.Topic;
+import com.aol.simple.react.stream.lazy.LazyFutureStream;
+import com.aol.simple.react.stream.lazy.LazyReact;
+import com.aol.simple.react.threads.ReactPool;
+
+/**
+ * Build Streams that stream data from the topic instance
+ * 
+ * @author johnmcclean
+ *
+ * @param <T>
+ */
+public class MultiplePushableStreamsBuilder<T>  {
+	
+	private final Topic<T> topic;
+
+	
+	MultiplePushableStreamsBuilder(Queue<T> q){
+		topic = new Topic(q);
+	}
+	
+	/**
+	 * Create a pushable LazyFutureStream using the supplied ReactPool
+	 * 
+	 * @param s ReactPool to use to create the Stream
+	 * @return a Tuple2 with a Topic<T> and LazyFutureStream<T> - add data to the Queue
+	 * to push it to the Stream
+	 */
+	public  LazyFutureStream<T> pushable(ReactPool<LazyReact> s){
+		
+		return s.nextReactor().fromStreamWithoutFutures(topic.stream());
+		
+	}
+
+	/**
+	 * Create a pushable JDK 8 Stream
+	 * @return a Tuple2 with a Topic<T> and Stream<T> - add data to the Queue
+	 * to push it to the Stream
+	 */
+	public   Stream<T> pushableStream(){
+		
+		return (Stream)topic.stream();
+		
+	}
+	/**
+	 * Create a pushable org.jooq.lambda.Seq
+	 * 
+	 * @return a Tuple2 with a Topic<T> and Seq<T> - add data to the Queue
+	 * to push it to the Stream
+	 */
+	public Seq<T> pushableSeq(){
+		
+		return topic.stream();
+	}
+	
+	/**
+	 * Create a pushable LazyFutureStream. This will call LazyFutureStream#futureStream(Stream) which creates
+	 * a sequential LazyFutureStream
+	 * 
+	  @return a Tuple2 with a Queue<T> and LazyFutureStream<T> - add data to the Queue
+	 * to push it to the Stream
+	 */
+	public  <T> LazyFutureStream<T> pushableLazyFutureStream(){
+		
+		return LazyFutureStream.futureStream((Stream<T>)topic.stream());
+	}
+
+	/**
+	 * @return Topic used as input for any generated Streams
+	 */
+	public Topic<T> getInput() {
+		return topic;
+	}
+
+}
---
+++ src/main/java/com/aol/simple/react/stream/pushable/PushableLazyFutureStream.java
@@ -0,0 +1,29 @@
+package com.aol.simple.react.stream.pushable;
+
+import java.util.stream.Stream;
+
+import org.jooq.lambda.tuple.Tuple2;
+
+import com.aol.simple.react.async.Queue;
+import com.aol.simple.react.stream.lazy.LazyFutureStream;
+
+/**
+ * A more concrete Tuple2 impl
+ * v1 is Queue<T>
+ * v2 is LazyFutureStream<T>
+ * 
+ * @author johnmcclean
+ *
+ * @param <T> data type
+ */
+public class PushableLazyFutureStream<T> extends AbstractPushableStream<T,Queue<T>,LazyFutureStream<T>> {
+
+	
+	public PushableLazyFutureStream(Queue<T> v1, LazyFutureStream<T> v2) {
+		super(v1, v2);
+		
+	}
+
+	private static final long serialVersionUID = 1L;
+
+}
