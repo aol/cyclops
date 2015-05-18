27d660b5726955ce916d3dc3667eba0749d8b4da
John McClean <john.mcclean@teamaol.com>
2015-05-18 12:41:45 +0100

    more javadoc

--- cyclops-core/src/main/java/com/aol/cyclops/value/AsStreamableValue.java
+++ cyclops-core/src/main/java/com/aol/cyclops/value/AsStreamableValue.java
@@ -12,10 +12,26 @@ import com.aol.cyclops.matcher.builders.PatternMatcher;
 import com.aol.cyclops.matcher.builders.SimplestCase;
 import com.aol.cyclops.matcher.builders._Case;
 
+/**
+ * Corerce an Object to implement the StreamableValue interface
+ * 
+ * This adds - support for decomposition, pattern matching, restreamable behaviour & for comprehensions to your objects
+ * 
+ * @author johnmcclean
+ *
+ */
 public class AsStreamableValue {
-	public static <T> StreamableValue<T> asValue(T toCoerce){
+	
+	/**
+	 * Coerce / wrap an Object as a StreamableValue instance
+	 * 
+	 * @param toCoerce Object to making into a StreamableValue
+	 * @return StreamableValue that delegates calls to the supplied object
+	 */
+	public static <T> StreamableValue<T> asStreamableValue(T toCoerce){
 		return new CoercedStreamableValue<T>(toCoerce);
 	}
+	
 	@lombok.Value
 	public static class CoercedStreamableValue<T> implements StreamableValue<T>{
 		private final T value;
