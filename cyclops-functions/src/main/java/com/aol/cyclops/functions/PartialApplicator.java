deab2669fba0856931cb715a4b7604a344251438
Brendan Carroll <brendan.carroll.2012@nuim.ie>
2015-06-24 18:31:12 +0100

    Added all function return types for partial applicators up to 8

--- cyclops-functions/src/main/java/com/aol/cyclops/functions/PartialApplicator.java
+++ cyclops-functions/src/main/java/com/aol/cyclops/functions/PartialApplicator.java
@@ -92,24 +92,28 @@ public class PartialApplicator extends Lambda{
     	return (t8) -> octFunc.apply(t1,t2,t3,t4,t5,t6,t7,t8);
     }
     
-/*    public static <T1,T2,T3,T4,T5,T6,T7,T8,R> BiFunction<T6,T7,R>  partial8(T1 t1, T2 t2, T3 t3, T4 t4, T5 t5, T6 t6, OctFunction<T1,T2,T3,T4,T5,T6,T7,T8,R> octFunc){
+    public static <T1,T2,T3,T4,T5,T6,T7,T8,R> BiFunction<T7,T8,R>  partial8(T1 t1, T2 t2, T3 t3, T4 t4, T5 t5, T6 t6, OctFunction<T1,T2,T3,T4,T5,T6,T7,T8,R> octFunc){
     	return (t7,t8) -> octFunc.apply(t1,t2,t3,t4,t5,t6,t7,t8);
     }
     
-    public static <T1,T2,T3,T4,T5,T6,T7,T8,R> TriFunction<T5,T6,T7,R>  partial8(T1 t1, T2 t2, T3 t3, T4 t4, T5 t5, OctFunction<T1,T2,T3,T4,T5,T6,T7,T8,R> octFunc){
+    public static <T1,T2,T3,T4,T5,T6,T7,T8,R> TriFunction<T6,T7,T8,R>  partial8(T1 t1, T2 t2, T3 t3, T4 t4, T5 t5, OctFunction<T1,T2,T3,T4,T5,T6,T7,T8,R> octFunc){
     	return (t6,t7,t8) -> octFunc.apply(t1,t2,t3,t4,t5,t6,t7,t8);
     }
     
-    public static <T1,T2,T3,T4,T5,T6,T7,T8,R> QuadFunction<T4,T5,T6,T7,R>  partial8(T1 t1, T2 t2, T3 t3, OctFunction<T1,T2,T3,T4,T5,T6,T7,T8,R> octFunc){
+    public static <T1,T2,T3,T4,T5,T6,T7,T8,R> QuadFunction<T5,T6,T7,T8,R>  partial8(T1 t1, T2 t2, T3 t3, T4 t4, OctFunction<T1,T2,T3,T4,T5,T6,T7,T8,R> octFunc){
     	return (t5,t6,t7,t8) -> octFunc.apply(t1,t2,t3,t4,t5,t6,t7,t8);
     }
  
-    public static <T1,T2,T3,T4,T5,T6,T7,T8,R> QuintFunction<T3,T4,T5,T6,T7,R>  partial8(T1 t1, T2 t2, OctFunction<T1,T2,T3,T4,T5,T6,T7,T8,R> octFunc){
+    public static <T1,T2,T3,T4,T5,T6,T7,T8,R> QuintFunction<T4,T5,T6,T7,T8,R>  partial8(T1 t1, T2 t2, T3 t3, OctFunction<T1,T2,T3,T4,T5,T6,T7,T8,R> octFunc){
         return (t4,t5,t6,t7,t8) -> octFunc.apply(t1,t2,t3,t4,t5,t6,t7,t8);
     }
     
-    public static <T1,T2,T3,T4,T5,T6,T7,T8,R> HexFunction<T2,T3,T4,T5,T6,T7,R>  partial8(T1 t1, OctFunction<T1,T2,T3,T4,T5,T6,T7,T8,R> octFunc){
-        return (t2,t3,t4,t5,t6,t7) -> octFunc.apply(t1,t2,t3,t4,t5,t6,t7,t8);
-    }*/
+    public static <T1,T2,T3,T4,T5,T6,T7,T8,R> HexFunction<T3,T4,T5,T6,T7,T8,R>  partial8(T1 t1, T2 t2, OctFunction<T1,T2,T3,T4,T5,T6,T7,T8,R> octFunc){
+        return (t3,t4,t5,t6,t7,t8) -> octFunc.apply(t1,t2,t3,t4,t5,t6,t7,t8);
+    }
+
+    public static <T1,T2,T3,T4,T5,T6,T7,T8,R> HeptFunction<T2,T3,T4,T5,T6,T7,T8,R>  partial8(T1 t1, OctFunction<T1,T2,T3,T4,T5,T6,T7,T8,R> octFunc){
+        return (t2,t3,t4,t5,t6,t7,t8) -> octFunc.apply(t1,t2,t3,t4,t5,t6,t7,t8);
+    }
 
 }
