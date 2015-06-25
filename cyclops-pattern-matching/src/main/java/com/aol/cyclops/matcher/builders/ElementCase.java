f4a69630eaa307c6fe13af6c70759c0471f01743
Castigliego, Marco <marco.castigliego@teamaol.com>
2015-06-25 12:35:47 +0100

    Wrapping multiple matchers with allOf from hamcrest

--- cyclops-pattern-matching/src/main/java/com/aol/cyclops/matcher/builders/ElementCase.java
+++ cyclops-pattern-matching/src/main/java/com/aol/cyclops/matcher/builders/ElementCase.java
@@ -1,5 +1,7 @@
 package com.aol.cyclops.matcher.builders;
 
+import static org.hamcrest.core.AllOf.allOf;
+
 import java.util.function.Predicate;
 
 import lombok.AccessLevel;
@@ -19,76 +21,85 @@ import com.aol.cyclops.matcher.builders.CaseBuilder.InMatchOfBuilder;
  * Pattern Matcher Case Builder for matching against a single element
  * 
  * @author johnmcclean
- *
  * @param <X>
  */
-@AllArgsConstructor(access=AccessLevel.PACKAGE)
-public class ElementCase<X> extends CaseBeingBuilt{
+@AllArgsConstructor(access = AccessLevel.PACKAGE)
+public class ElementCase<X> extends CaseBeingBuilt {
 	@Getter(AccessLevel.PACKAGE)
 	@Wither(AccessLevel.PACKAGE)
 	private final PatternMatcher patternMatcher;
-	
-	
+
 	/**
-	 * Build a Case which is triggered when the user input matches the supplied Value (via Objects.equals) 
+	 * Build a Case which is triggered when the user input matches the supplied Value (via Objects.equals)
 	 * 
-	 * 
-	 * @param value will be compared to input provided in match method
+	 * @param value
+	 *            will be compared to input provided in match method
 	 * @return Next step in this Case builder
 	 */
-	public  <V> Step<V,X> isValue(V value){
-		
+	public <V> Step<V, X> isValue(V value) {
+
 		return new CaseBuilder.ValueStep<>(this, patternMatcher, value);
-		
+
 	}
-	
-	
-	
+
 	/**
-	 * Create a completed Case which is triggered when matching input is of the same type (T) as the input parameter
-	 *  to ActionWithReturn.
-	 *  
-	 *  The ActionWithReturn will then be executed and the result returned as the match result.
+	 * Create a completed Case which is triggered when matching input is of the same type (T) as the input parameter to ActionWithReturn. The
+	 * ActionWithReturn will then be executed and the result returned as the match result.
 	 * 
-	 * @param a Action for the new Case, Predicate for the Case will be created from the input type to the Action.
+	 * @param a
+	 *            Action for the new Case, Predicate for the Case will be created from the input type to the Action.
 	 * @return Completed Case
 	 */
-	public  <T,R> MatchingInstance<T,R> isType(ActionWithReturn<T,R> a){
-		
+	public <T, R> MatchingInstance<T, R> isType(ActionWithReturn<T, R> a) {
+
 		return new MatchingInstance<>(this.withPatternMatcher(patternMatcher.inCaseOfType(a)));
-		
+
 	}
+
 	/**
 	 * Build a Case which is triggered when the supplied Predicate holds
 	 * 
-	 * @param match Predicate which will trigger this case
+	 * @param match
+	 *            Predicate which will trigger this case
 	 * @return Next Step in the Case Builder process
 	 */
-	public  <V> InCaseOfBuilder<V> isTrue(Predicate<V> match){
-		return new InCaseOfBuilder<V>(match,patternMatcher,this);
+	public <V> InCaseOfBuilder<V> isTrue(Predicate<V> match) {
+		return new InCaseOfBuilder<V>(match, patternMatcher, this);
 	}
-	
+
 	/**
-	 *  Build a Case which is triggered when the supplied Hamcrest Matcher holds
+	 * Build a Case which is triggered when the supplied Hamcrest Matcher holds
 	 * 
-	 * @param match Hamcrest Matcher that will trigger this case
+	 * @param match
+	 *            Hamcrest Matcher that will trigger this case
 	 * @return Next Step in the Case Builder process
 	 */
-	public  <V> InMatchOfBuilder<V,X> isMatch(Matcher<V> match){
-		
-		return new InMatchOfBuilder<V,X>(match,patternMatcher,this);
+	public <V> InMatchOfBuilder<V, X> isMatch(Matcher<V> match) {
+
+		return new InMatchOfBuilder<V, X>(match, patternMatcher, this);
 	}
-	
-	
-	
+
+	/**
+	 * Build a Case which is triggered when the supplied Hamcrest Matchers holds
+	 * 
+	 * @param match
+	 *            Hamcrest Matchers that will trigger this case
+	 * @return Next Step in the Case Builder process
+	 */
+	public <V> InMatchOfBuilder<V, X> isMatch(Matcher<V>... matches) {
+
+		return new InMatchOfBuilder<V, X>(allOf(matches), patternMatcher, this);
+	}
+
 	/**
 	 * Preprocess the user supplied matching data in some way before it meets the predicate or action for this Case
 	 * 
-	 * @param extractor Extractor to preprocess input data
+	 * @param extractor
+	 *            Extractor to preprocess input data
 	 * @return Next step in the Case Builder process
 	 */
-	public  <T,R,X> InCaseOfBuilderExtractor<T,R,X> extract( Extractor<T,R> extractor){
-		return new InCaseOfBuilderExtractor<T,R,X>(extractor,patternMatcher,this);
+	public <T, R, X> InCaseOfBuilderExtractor<T, R, X> extract(Extractor<T, R> extractor) {
+		return new InCaseOfBuilderExtractor<T, R, X>(extractor, patternMatcher, this);
 	}
 
 }
--- cyclops-pattern-matching/src/test/java/com/aol/cyclops/matcher/HamcrestTest.java
+++ cyclops-pattern-matching/src/test/java/com/aol/cyclops/matcher/HamcrestTest.java
@@ -1,16 +1,20 @@
 package com.aol.cyclops.matcher;
 
 import static com.aol.cyclops.matcher.Extractors.get;
+import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.hasItem;
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import org.hamcrest.Matcher;
 import org.junit.Test;
@@ -21,49 +25,58 @@ import com.aol.cyclops.matcher.builders.PatternMatcher;
 public class HamcrestTest {
 
 	@Test
-	public void hamcrest1(){
-		Matching.whenValues(c->c.isMatch(hasItem("hello2")).thenConsume(System.out::println))
-							.match(Arrays.asList("hello","world"));
+	public void hamcrest1() {
+		Matching.whenValues(c -> c.isMatch(hasItem("hello2")).thenConsume(System.out::println)).match(Arrays.asList("hello", "world"));
 	}
+
 	@Test
-	public void hamcrestViaNew(){
-		Matching.when().isMatch(hasItem("hello2")).thenConsume(System.out::println)
-				.when().isMatch(contains("world","hello","unexpected")).thenConsume(System.err::println)
-							.match(Arrays.asList("hello","world"));
+	public void multipleHamcrestMatchers() {
+		assertThat(
+				Matching.when().isMatch(containsString("hello"), containsString("world")).thenApply(message -> message).match("hello world").get(),
+				is("hello world"));
+
+		assertThat(Matching.when().isMatch(containsString("hello"), containsString("universe")).thenApply(message -> message).match("hello world"),
+				is(Optional.empty()));
 	}
+
 	@Test
-	public void hasAnyOrderTest(){
-		assertTrue(Matching.<Boolean>whenValues(c->c.isMatch(containsInAnyOrder("world","hello"))
-											.thenApply(list -> true))
-							.apply(Arrays.asList("hello","world")).get());
+	public void hamcrestViaNew() {
+		Matching.when().isMatch(hasItem("hello2")).thenConsume(System.out::println).when().isMatch(contains("world", "hello", "unexpected"))
+				.thenConsume(System.err::println).match(Arrays.asList("hello", "world"));
 	}
+
 	@Test
-	public void hamcrestWithExtractor(){
-		assertTrue(Matching.when().extract(Extractors.at(1))
-										.isMatch(is("world"))
-										.thenApply(list -> true)
-							.match(Arrays.asList("hello","world")).get());
+	public void hasAnyOrderTest() {
+		assertTrue(Matching.<Boolean> whenValues(c -> c.isMatch(containsInAnyOrder("world", "hello")).thenApply(list -> true))
+				.apply(Arrays.asList("hello", "world")).get());
 	}
+
 	@Test
-	public void postExtractorDoc(){
-		Matching.when().isMatch(is(not(empty()))).thenExtract(get(1))
-					.thenConsume(System.out::println)
-				.match(Arrays.asList(20303,"20303 is passing",true));
+	public void hamcrestWithExtractor() {
+		assertTrue(Matching.when().extract(Extractors.at(1)).isMatch(is("world")).thenApply(list -> true).match(Arrays.asList("hello", "world"))
+				.get());
 	}
+
 	@Test
-	public void hamcrestWithPostExtractor(){
-		assertEquals("world",Matching.whenValues(c->c.isMatch(containsInAnyOrder("world","hello"))
-													.thenExtract(Extractors.at(1))
-													.thenApply(value -> value))
-					
-							.apply(Arrays.asList("hello","world")).get());
+	public void postExtractorDoc() {
+		Matching.when().isMatch(is(not(empty()))).thenExtract(get(1)).thenConsume(System.out::println)
+				.match(Arrays.asList(20303, "20303 is passing", true));
 	}
+
 	@Test
-	public void hamcrestWithPostExtractor2(){
-		assertEquals("world",new PatternMatcher().<String,List<String>,String>inMatchOfThenExtract(
-				(Matcher)containsInAnyOrder("world","hello"),
-										(String value) -> value
-									,(List<String> list)-> list.get(1))
-							.apply(Arrays.asList("hello","world")).get());
+	public void hamcrestWithPostExtractor() {
+		assertEquals("world",
+				Matching.whenValues(c -> c.isMatch(containsInAnyOrder("world", "hello")).thenExtract(Extractors.at(1)).thenApply(value -> value))
+
+				.apply(Arrays.asList("hello", "world")).get());
+	}
+
+	@Test
+	public void hamcrestWithPostExtractor2() {
+		assertEquals(
+				"world",
+				new PatternMatcher()
+						.<String, List<String>, String> inMatchOfThenExtract((Matcher) containsInAnyOrder("world", "hello"), (String value) -> value,
+								(List<String> list) -> list.get(1)).apply(Arrays.asList("hello", "world")).get());
 	}
 }
