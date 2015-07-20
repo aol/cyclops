f2ac78d3cd3b1ec6c984b179cf63dfb6f9c082ae
John McClean <john.mcclean@teamaol.com>
2015-07-20 14:13:34 +0100

    added notes

--- cyclops-functionaljava/src/test/java/com/aol/cyclops/functionaljava/reader/DITest.java
+++ cyclops-functionaljava/src/test/java/com/aol/cyclops/functionaljava/reader/DITest.java
@@ -2,6 +2,7 @@ package com.aol.cyclops.functionaljava.reader;
 
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.Matchers.*;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,6 +12,7 @@ import org.junit.Test;
 //http://blog.originate.com/blog/2013/10/21/reader-monad-for-dependency-injection/
 public class DITest {
 
+	@SuppressWarnings("serial")
 	Map<String,String> map = new HashMap<String,String> (){{
 									put("fullname","bob");
 									put("email","bob@user.com");
@@ -21,3 +23,8 @@ public class DITest {
 		assertThat(new Application().userInfo("bob"), equalTo(map));
 	}
 }
+
+/** NOTES : See UserInfo class where current main compromise occurs.
+UserRepository type info temporarily lost & locally restored. This could be solved
+by a custom For Comprehension builder for FJ. Would also make construction of For Comphrensions
+a little less clunky**/
