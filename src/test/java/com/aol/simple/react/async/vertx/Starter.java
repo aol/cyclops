65405f020da8b3b0a87fe8d221fd865e92e171e4
John McClean <john.mcclean@teamaol.com>
2015-08-25 16:58:44 +0100

    add in vert.x tests

---
+++ src/test/java/com/aol/simple/react/async/vertx/Starter.java
@@ -0,0 +1,23 @@
+package com.aol.simple.react.async.vertx;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Vertx;
+
+import com.aol.simple.react.stream.lazy.LazyReact;
+
+public class Starter extends AbstractVerticle {
+	  @Override
+	  public void start() throws Exception {
+	    super.start();
+	    
+	    LazyReact react = new LazyReact(new VertxExecutor(getVertx()));
+	    int number = react.of(1, 2, 3).map(i -> i + 1).reduce((a,b) -> a + b).orElse(Integer.MIN_VALUE);
+	    System.out.println("sum = " + number); // 2 + 3 + 4 = 9
+	    // works with blocking thread pool
+	    // does not work with non-blocking event loop
+	  }
+
+	  public static void main(String[] args) {
+	    Vertx.vertx().deployVerticle(Starter.class.getCanonicalName());
+	  }
+	}
