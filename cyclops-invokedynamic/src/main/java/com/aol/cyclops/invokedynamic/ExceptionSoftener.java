19bd08c75496be98afa1d732fe7a1382d601f15b
John McClean <john.mcclean@teamaol.com>
2015-10-29 13:34:15 +0000

    restore after merge error

--- cyclops-invokedynamic/src/main/java/com/aol/cyclops/invokedynamic/ExceptionSoftener.java
+++ cyclops-invokedynamic/src/main/java/com/aol/cyclops/invokedynamic/ExceptionSoftener.java
@@ -1,22 +1,11 @@
 package com.aol.cyclops.invokedynamic;
 
-import java.io.IOException;
 
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class ExceptionSoftener {
 
-<<<<<<< HEAD
-	public static <T extends Throwable,R> R throwSoftenedException(final T e) {
-		uncheck(e);
-		return null;
-	}
-
-	private static <T extends Throwable> void uncheck(Throwable throwable) throws T {
-		throw (T) throwable;
-
-=======
 	
 	
 	
@@ -27,11 +16,10 @@ public class ExceptionSoftener {
 	@SuppressWarnings("unchecked")
 	private static <T extends Throwable> T uncheck(Throwable throwable) throws T {
 		throw (T) throwable;
->>>>>>> 541bfbd5f5ab031ffed5125fc0061194c124f340
 	}
+			 
+			
 	
-	public static Integer myTest(){
-		return throwSoftenedException(new IOException());
-	}
 
 }
+
