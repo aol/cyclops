896968b300f06efd0bc8164cf503847dcb6b176f
John McClean <john.mcclean@teamaol.com>
2015-12-15 21:53:40 +0000

    javadoc

--- cyclops-functions/src/main/java/com/aol/cyclops/functions/caching/Cacheable.java
+++ cyclops-functions/src/main/java/com/aol/cyclops/functions/caching/Cacheable.java
@@ -1,8 +1,37 @@
 package com.aol.cyclops.functions.caching;
 
+import static com.aol.cyclops.functions.caching.Memoize.memoizeBiFunction;
+
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
+/**
+ * Interface that represents a pluggable cache
+ * 
+ * @author johnmcclean
+ * 
+ * E.g. plugging in a Guava cache
+ * <pre>
+ * {@code 
+ * Cache<Object, String> cache = CacheBuilder.newBuilder()
+       .maximumSize(1000)
+       .expireAfterWrite(10, TimeUnit.MINUTES)
+       .build();
+
+ * BiFunction<Integer,Integer,Integer> s = Memoize.memoizeBiFunction( (a,b)->a + ++called,
+ * 													(key,fn)-> cache.get(key,()->fn.apply(key)); 
+ * }
+ * </pre>
+ *
+ * @param <OUT>
+ */
 public interface Cacheable<OUT> {
 
+	/**
+	 * Implementation should call the underlying cache
+	 * @param key To lookup cached value
+	 * @param fn Function to compute value, if it is not in the cache
+	 * @return Cached (or computed) result
+	 */
 	public OUT computeIfAbsent(Object key, Function<Object,OUT> fn);
 }
