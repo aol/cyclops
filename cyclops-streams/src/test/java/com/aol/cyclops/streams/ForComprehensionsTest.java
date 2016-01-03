36005006e4ed64846572a2d8037723e5621b45b6
John McClean <john.mcclean@teamaol.com>
2016-01-03 00:15:53 +0000

    more unit tests

--- cyclops-streams/src/test/java/com/aol/cyclops/streams/ForComprehensionsTest.java
+++ cyclops-streams/src/test/java/com/aol/cyclops/streams/ForComprehensionsTest.java
@@ -17,11 +17,7 @@ public class ForComprehensionsTest {
 
 	@Test
 	public void forEach2(){
-		Stream<Double> s1 =  Do.addStream(Stream.of(1,2,3))
-				.withBaseStream(u->IntStream.range(0,10))
-				//.withStream(d->IntStream.range(0,10).boxed())
-				.yield( a -> b-> a+b).unwrap();
-		System.out.println(s1.collect(Collectors.toList()));
+		
 		
 		List<Integer> s = Do.add(Arrays.asList(1,2,3))
 								.withBaseStream(u->IntStream.range(0,10))
@@ -35,5 +31,24 @@ public class ForComprehensionsTest {
 		        		 a->b-> a+b)
 		         .toList(),equalTo(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 2, 3, 4, 5, 6, 7, 8, 
 		        		 9, 10, 11, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)));
+	}
+	@Test
+	public void intStreamSanityCheck() {
+		Stream<Double> s1 =  Do.addStream(Stream.of(1,2,3))
+				.withBaseStream(u->IntStream.range(0,10))
+				//.withStream(d->IntStream.range(0,10).boxed())
+				.yield( a -> b-> a+b).unwrap();
+		System.out.println(s1.collect(Collectors.toList()));
+	}
+	@Test
+	public void forEach2Filter(){
+
+		
+		
+		assertThat(SequenceM.of(1,2,3)
+		         .forEach2(a->IntStream.range(0,10), 
+		        		 a->b->a>2 && b<8,
+		        		 a->b-> a+b)
+		         .toList(),equalTo(Arrays.asList(3,4,5,6,7,8,9,10)));
 	}
 }
