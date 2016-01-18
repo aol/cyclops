a64abbb51933aaca0c2a6644782dfa33a64554f4
John McClean <john.mcclean@teamaol.com>
2016-01-18 22:05:46 +0000

    passing tests

--- src/test/java/com/aol/simple/react/monad/DoTest.java
+++ src/test/java/com/aol/simple/react/monad/DoTest.java
@@ -1,7 +1,7 @@
 package com.aol.simple.react.monad;
 
 import static org.hamcrest.Matchers.equalTo;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -65,12 +65,13 @@ public class DoTest {
 	
 	@Test
 	public void doTestLazyOptionalEmptyStream(){
+		
 		Optional<List<Integer>> result = Do.add(lookup("1"))
 												.add(LazyFutureStream.<Integer>of())
 												.yield((Integer a) -> (Integer b) -> a+b)
 												.unwrap();
 		System.out.println(result);
-		assertThat(result.get().size(),equalTo(0));
+		assertTrue(!result.isPresent());
 	}
 	@Test
 	public void doTestSimpleOptionalEmptyStream(){
