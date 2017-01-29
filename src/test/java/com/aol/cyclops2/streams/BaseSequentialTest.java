0cbfd22db7f55881dfdc61fb7d317c77155d63eb
John McClean <john.mcclean@teamaol.com>
2017-01-29 20:44:20 +0000

    bug fixing

--- src/test/java/com/aol/cyclops2/streams/BaseSequentialTest.java
+++ src/test/java/com/aol/cyclops2/streams/BaseSequentialTest.java
@@ -177,7 +177,7 @@ public class BaseSequentialTest {
 
                 try {
                     System.out.println("Sleeping!");
-                    Thread.sleep(10);
+                    Thread.sleep(100);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
