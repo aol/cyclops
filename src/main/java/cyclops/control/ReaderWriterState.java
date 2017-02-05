dcea876015e4aec842baaecee7eabbad2e7fc3d6
John McClean <john.mcclean@teamaol.com>
2017-02-05 11:21:20 +0000

    ReaderWriterState monad

--- src/main/java/cyclops/control/ReaderWriterState.java
+++ src/main/java/cyclops/control/ReaderWriterState.java
@@ -2,41 +2,48 @@ package cyclops.control;
 
 import cyclops.free.Free;
 import cyclops.function.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import org.jooq.lambda.tuple.Tuple;
 import org.jooq.lambda.tuple.Tuple2;
+import org.jooq.lambda.tuple.Tuple3;
 
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
-/**
- * Created by johnmcclean on 05/02/2017.
- */
+
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class ReaderWriterState<R,W,S,T>  {
 
+    Monoid<W> monoid;
+    BiFunction<R,S, Free<Fn0.SupplierKind.µ,Tuple3<W, S, T>>> runState;
 
-    Reader<S, Free<Fn0.SupplierKind.µ,Tuple2<Writer<W, S>, T>>> runState;
-
-    public Tuple2<Writer<W,S>,T> run(S s) {
-        return Fn0.run(runState.apply(s));
-    }
-    public T eval(S s) {
-        return Fn0.run(runState.apply(s)).v2;
-    }
-    public <R> State<S, R> map(Function<? super T,? extends R> mapper) {
-        return mapState(t -> Tuple.tuple(t.v1, mapper.apply(t.v2)));
-    }
-    public <R> State<S, R> mapState(Function<Tuple2<S,T>, Tuple2<S, R>> fn) {
-        return suspended(s -> runState.apply(s).map(t -> fn.apply(t.map((w,r)->w.getValue().map2(__->r)))));
-    }
-    private static <S, T> State<S, T> suspended(Fn1<? super S, Free<Fn0.SupplierKind.µ,Tuple2<S, T>>> runF) {
-        return new State<>(s -> Fn0.suspend(Lambda.λK(()->runF.apply(s))));
+    public Tuple3<W,S,T> run(R r,S s) {
+        return Fn0.run(runState.apply(r,s));
     }
 
-    public <R> State<S, R> flatMap(Function<? super T,? extends  State<S, R>> f) {
-        return suspended(s -> runState.apply(s).flatMap(result -> Free.done(f.apply(result.v2).run(result.v1))));
+    public <R2> ReaderWriterState<R,W,S,R2> map(Function<? super T,? extends R2> mapper) {
+
+        return mapState(t -> Tuple.tuple(t.v1, t.v2, mapper.apply(t.v3)));
+    }
+    public <R2> ReaderWriterState<R,W,S,R2> mapState(Function<Tuple3<W,S,T>, Tuple3<W,S, R2>> fn) {
+        return suspended((r,s) -> runState.apply(r,s).map(t3 -> fn.apply(t3)),monoid);
+    }
+    private static <R,W,S,T> ReaderWriterState<R,W,S,T> suspended(BiFunction<? super R, ? super S, Free<Fn0.SupplierKind.µ,Tuple3<W,S, T>>> runF,
+                                                                  Monoid<W> monoid) {
+
+        return new ReaderWriterState<R, W, S, T>(monoid,(R r ,S s) -> Fn0.suspend(Lambda.λK(()->runF.apply(r,s))));
+
     }
 
-    public static <R,W,S,T> ReaderWriterState<R,W,S,T> rws(Function<? super S,? extends Tuple2<S, T>> runF, Monoid<W> monoid) {
+    public <R2> ReaderWriterState<R,W,S,R2> flatMap(Function<? super T,? extends  ReaderWriterState<R,W,S,R2>> f) {
 
-        return new State<>(s -> Free.done(runF.apply(s)));
+        return suspended((r,s) -> runState.apply(r, s)
+                .flatMap(result -> Free.done(f.apply(result.v3).run(r, result.v2))),monoid);
+    }
+
+    public static <R,W,S,T> ReaderWriterState<R,W,S,T> rws(BiFunction<? super R, ? super S,? extends Tuple3<W,S, T>> runF, Monoid<W> monoid) {
+
+        return new ReaderWriterState<R, W, S, T>(monoid,(r,s) -> Free.done(runF.apply(r,s)));
     }
 }
