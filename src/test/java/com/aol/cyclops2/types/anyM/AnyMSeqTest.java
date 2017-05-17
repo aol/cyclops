b4d3db2b1c4b74d07dead5083f9cedc0ccfc1ae2
William Yang <weili.yang@teamaol.com>
2017-05-17 16:36:37 +0100

    added unit test case

--- src/test/java/com/aol/cyclops2/types/anyM/AnyMSeqTest.java
+++ src/test/java/com/aol/cyclops2/types/anyM/AnyMSeqTest.java
@@ -25,7 +25,17 @@ public class AnyMSeqTest {
 		AnyMSeq merged = seq.mergeP(QueueFactories.unboundedQueue(), FutureStream.builder().from(ListX.of(40, 50, 60)));
 		assertEquals(4, merged.count());
 		merged.traversable().forEach(x -> {
-			System.out.println(x);
+			Assert.assertTrue(expected.contains(x));
+		});
+	}
+	
+	@Test
+	public void testMergeP2() {
+		List<Integer> expected = Arrays.asList(new Integer[] { 10, 40, 50, 60 });
+		AnyMSeq seq = new AnyMSeqImpl(Optional.of(10), OptionalAdapter.optional);
+		AnyMSeq merged = seq.mergeP(FutureStream.builder().from(ListX.of(40, 50, 60)));
+		assertEquals(4, merged.count());
+		merged.traversable().forEach(x -> {
 			Assert.assertTrue(expected.contains(x));
 		});
 	}
