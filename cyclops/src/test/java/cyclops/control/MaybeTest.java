d96eb69b392dce5c2015d335a505541b92c7671c
John McClean <john.mcclean@teamaol.com>
2017-12-14 11:15:36 +0000

    impls for #797 and #798

--- cyclops/src/test/java/cyclops/control/MaybeTest.java
+++ cyclops/src/test/java/cyclops/control/MaybeTest.java
@@ -47,7 +47,24 @@ public class MaybeTest extends  AbstractValueTest implements Printable {
 
     int cap =0;
 
+  @Test
+  public void testMaybeWithNull() {
+    System.out.println(Maybe.of(null).toString());
+    System.out.println(Maybe.just(null).toString());
+    System.out.println(Either.left(null).toString());
+    System.out.println(Either.right(null).toString());
 
+    System.out.println(Eval.defer(null).toString());
+    System.out.println(Eval.later(null).toString());
+    System.out.println(LazyEither5.left1(null).toString());
+    System.out.println(LazyEither5.left2(null).toString());
+    System.out.println(LazyEither5.left3(null).toString());
+    System.out.println(LazyEither5.left4(null).toString());
+    System.out.println(LazyEither5.right(null).toString());
+
+    System.out.println(Option.some(null).toString());
+    System.out.println(Option.of(null).toString());
+  }
     @Test
     public void lazy(){
 
