6dcc33f416da8523a7e0f91de008c0ab95a8403b
Maxwell Chiareli <maxwell.chiareli.xandeco@teamaol.com>
2018-04-11 16:05:26 +0100

    rename matching api from of to with
    rename 'of'  method to 'with'
    fix tests
    change Case api to use a Function instead of Supplier
    create overload methods to allow use of Supplier when creating Case instances

--- cyclops/src/test/java/com/oath/cyclops/matching/PatternMatchingTest.java
+++ cyclops/src/test/java/com/oath/cyclops/matching/PatternMatchingTest.java
@@ -11,49 +11,46 @@ import java.util.Random;
 
 public class PatternMatchingTest {
 
-  private static final Random random = new Random();
+    private static final Random random = new Random();
 
-  private Long value;
-  private Matching.PatternMatching<Long> sumMatching;
+    private Long value;
+    private Matching.PatternMatching<Long> sumMatching;
 
-  @Before
-  public void before() {
-    value = random.nextLong();
-    sumMatching = new Matching.PatternMatching<>(value);
-  }
+    @Before
+    public void before() {
+        value = random.nextLong();
+        sumMatching = new Matching.PatternMatching<>(value);
+    }
 
-  @Test
-  public void shouldReturnAny() throws Exception {
-    assertEquals("any", sumMatching.of(Case("1"), Case("2"), () -> "any"));
-    assertEquals("any", sumMatching.of(Case("1"), Case("2"), Case("3"), () -> "any"));
-    assertEquals("any", sumMatching.of(Case("1"), Case("2"), Case("3"), Case("4"), () -> "any"));
-    assertEquals("any", sumMatching.of(Case("1"), Case("2"), Case("3"), Case("4"), Case("5"), () -> "any"));
-    assertEquals("any", sumMatching.of(Case("1"), Case("2"), Case("3"), Case("4"), Case("5"), Case("6"), () -> "any"));
-    assertEquals("any", sumMatching.of(Case("1"), Case("2"), Case("3"), Case("4"), Case("5"), Case("6"), Case("7"), () -> "any"));
-    assertEquals("any", sumMatching.of(Case("1"), Case("2"), Case("3"), Case("4"), Case("5"), Case("6"), Case("7"), Case("8"), () -> "any"));
-  }
+    @Test
+    public void shouldReturnAny() throws Exception {
+        assertEquals("any", sumMatching.with(Case("1"), Case("2"), value -> "any"));
+        assertEquals("any", sumMatching.with(Case("1"), Case("2"), Case("3"), value -> "any"));
+        assertEquals("any", sumMatching.with(Case("1"), Case("2"), Case("3"), Case("4"), value -> "any"));
+        assertEquals("any", sumMatching.with(Case("1"), Case("2"), Case("3"), Case("4"), Case("5"), value -> "any"));
+        assertEquals("any", sumMatching.with(Case("1"), Case("2"), Case("3"), Case("4"), Case("5"), Case("6"), value -> "any"));
+        assertEquals("any", sumMatching.with(Case("1"), Case("2"), Case("3"), Case("4"), Case("5"), Case("6"), Case("7"), value -> "any"));
+        assertEquals("any", sumMatching.with(Case("1"), Case("2"), Case("3"), Case("4"), Case("5"), Case("6"), Case("7"), Case("8"), value -> "any"));
+    }
 
-  @Test
-  public void shouldReturnTheMatch() throws Exception {
-    assertEquals("1", sumMatching.of(Case(value, "1"), Case("2"), () -> "any"));
-    assertEquals("2", sumMatching.of(Case("1"), Case(value, "2"), Case("3"), () -> "any"));
-    assertEquals("3", sumMatching.of(Case("1"), Case("2"), Case(value, "3"), Case("4"), () -> "any"));
-    assertEquals("4", sumMatching.of(Case("1"), Case("2"), Case("3"), Case(value, "4"), Case("5"), () -> "any"));
-    assertEquals("5", sumMatching.of(Case("1"), Case("2"), Case("3"), Case("4"), Case(value, "5"), Case(value, "6"), () -> "any"));
-    assertEquals("6", sumMatching.of(Case("1"), Case("2"), Case("3"), Case("4"), Case("5"), Case(value, "6"), () -> "any"));
-    assertEquals("7", sumMatching.of(Case("1"), Case("2"), Case("3"), Case("4"), Case("5"), Case("6"), Case(value, "7"), Case("8"), () -> "any"));
-    assertEquals("8", sumMatching.of(Case("1"), Case("2"), Case("3"), Case("4"), Case("5"), Case("6"), Case("7"), Case(value, "8"), () -> "any"));
-  }
+    @Test
+    public void shouldReturnTheMatch() throws Exception {
+        assertEquals("1", sumMatching.with(Case(value, "1"), Case("2"), value -> "any"));
+        assertEquals("2", sumMatching.with(Case("1"), Case(value, "2"), Case("3"), value -> "any"));
+        assertEquals("3", sumMatching.with(Case("1"), Case("2"), Case(value, "3"), Case("4"), value -> "any"));
+        assertEquals("4", sumMatching.with(Case("1"), Case("2"), Case("3"), Case(value, "4"), Case("5"), value -> "any"));
+        assertEquals("5", sumMatching.with(Case("1"), Case("2"), Case("3"), Case("4"), Case(value, "5"), Case(value, "6"), value -> "any"));
+        assertEquals("6", sumMatching.with(Case("1"), Case("2"), Case("3"), Case("4"), Case("5"), Case(value, "6"), value -> "any"));
+        assertEquals("7", sumMatching.with(Case("1"), Case("2"), Case("3"), Case("4"), Case("5"), Case("6"), Case(value, "7"), Case("8"), value -> "any"));
+        assertEquals("8", sumMatching.with(Case("1"), Case("2"), Case("3"), Case("4"), Case("5"), Case("6"), Case("7"), Case(value, "8"), value -> "any"));
+    }
 
 
-  Case0<Long, String> Case(String out) {
-    return Case(random.nextLong(), out);
-  }
+    Case0<Long, String> Case(String out) {
+        return Case(random.nextLong(), out);
+    }
 
-  Case0<Long, String> Case(Long in, String out) {
-    return new Case0<>(
-        t -> t.equals(in),
-        () -> out
-    );
-  }
+    Case0<Long, String> Case(Long in, String out) {
+        return new Case0<>(t -> t.equals(in), (value) -> out);
+    }
 }
