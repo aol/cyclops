5b3fec1db78b1d6a6f158e4452890a13db533802
johnmcclean <john.mcclean@teamaol.com>
2019-06-19 21:06:31 +0100

    Fix for #1058

--- cyclops-jackson-integration/src/main/java/com/oath/cyclops/jackson/serializers/Tuple6Serializer.java
+++ cyclops-jackson-integration/src/main/java/com/oath/cyclops/jackson/serializers/Tuple6Serializer.java
@@ -23,7 +23,7 @@ public class Tuple6Serializer extends JsonSerializer<Tuple6<?,?,?,?,?,?>> {
     Object[] array = new Object[]{value._1(),value._2(),value._3(),value._4(),value._5(),value._6()};
     gen.writeStartArray();
     for(Object o : array) {
-      JsonSerializer<Object> ser = serializers.findValueSerializer(o.getClass());
+      JsonSerializer<Object> ser = serializers.findTypedValueSerializer(o.getClass(),true,null);
       ser.serialize(o, gen, serializers);
     }
     gen.writeEndArray();
