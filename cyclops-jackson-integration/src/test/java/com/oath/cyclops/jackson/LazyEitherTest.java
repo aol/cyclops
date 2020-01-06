9029b857548099be03724c93f7465c95cadedace
johnmcclean <john.mcclean@teamaol.com>
2020-01-06 13:55:15 +0000

    Fix for #1090

--- cyclops-jackson-integration/src/test/java/com/oath/cyclops/jackson/LazyEitherTest.java
+++ cyclops-jackson-integration/src/test/java/com/oath/cyclops/jackson/LazyEitherTest.java
@@ -2,6 +2,7 @@ package com.oath.cyclops.jackson;
 
 import cyclops.control.Either;
 import cyclops.control.LazyEither;
+import cyclops.control.Option;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -20,7 +21,31 @@ public class LazyEitherTest {
     assertThat(JacksonUtil.serializeToJson(LazyEither.right(10)),equalTo("{\"right\":10}"));
 
   }
-
+    @Test
+    public void infiniteEither() {
+        Either<Exception,String> result = Option.<String>none().toTry(new Exception("asdf")).asEither();
+        for (int i = 0; i < 10; i++) {
+            result = result.flatMapLeftToRight(e -> Either.left(new Exception("asdf")));
+        }
+        System.out.println(result.toString());
+    }
+    @Test
+    public void infiniteEitherRight() {
+        Either<Exception,String> result = Option.<String>none().toTry(new Exception("asdf")).asEither();
+        //for (int i = 0; i < 10; i++)
+        {
+            result = result.flatMapLeft(e -> Either.right("asdf"));
+        }
+        System.out.println(result.toString());
+    }
+    @Test
+    public void infiniteEitherFlatMap() {
+        Either<Exception,String> result = Option.<String>none().toTry(new Exception("asdf")).asEither();
+        for (int i = 0; i < 10; i++) {
+            result = result.flatMap(e -> Either.left(new Exception("asdf")));
+        }
+        System.out.println(result.toString());
+    }
   @Test
   public void roundTripLeft(){
 
